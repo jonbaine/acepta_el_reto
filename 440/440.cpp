ce1cac50e3a8b861b2b59d4ebca58fc355b9ea5e
jonbaine <jonbaine@gmail.com>
2020-11-10 11:21:05 +0100

    Add cin magic.

--- 440/440.cpp
+++ 440/440.cpp
@@ -1,41 +1,41 @@
 
 #include <bits/stdc++.h>
 
-int main (int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
   for (;;) {
     int max = INT_MIN;
     int min = INT_MAX;
     int currentSpeed, minSpeed, elements;
     int groups = 1, currentGroupSize = 1;
 
-      std::cin >> elements;
-      if (!elements){
-	break;
+    std::cin >> elements;
+    if (!elements){
+      break;
+    }
+    std::cin >> minSpeed;
+    for (int i = 0; i < elements - 1; i++){
+      std::cin >> currentSpeed;
+      if (minSpeed > currentSpeed){
+        groups++;
+        if (currentGroupSize > max)
+          max = currentGroupSize;
+        if (currentGroupSize < min)
+          min = currentGroupSize;
+        currentGroupSize = 1;
+        minSpeed = currentSpeed;
+      }else{
+        currentGroupSize++;
       }
+    }
 
-      std::cin >> minSpeed;
-      for (int i=0; i < elements-1; i++){
-	std::cin >> currentSpeed;
-	if (minSpeed > currentSpeed){
-	  groups++;
-	  if (currentGroupSize > max)
-	    max = currentGroupSize;
-	  if (currentGroupSize < min)
-	    min = currentGroupSize;
-	  currentGroupSize = 1;
-	  minSpeed = currentSpeed;
-	} else {
-	  currentGroupSize++;
-	}
-
-      }
-    
     if (currentGroupSize > max)
       max = currentGroupSize;
     if (currentGroupSize < min)
       min = currentGroupSize;
 
-    std::cout << groups << " " << min << " " << max <<std::endl;
-    
+    std::cout << groups << " " << min << " " << max << std::endl;
   }
 }
